6eae88049610f5a0d41795ce72a29cbb67d9a7de
Amin Mohamadi <amindevzone@gmail.com>
2025-06-13 09:02:25 +0330

    Write bootstrap file as the starter of the project

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+define("URL_ROOT", "http://localhost/php-mvc");
+define("BASE_DIR", __DIR__);
+
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/config/Database.php";
